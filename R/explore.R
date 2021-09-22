c3998c61ce87213f4307db8546e0bfe31fcec162
Peter Solymos <psolymos@gmail.com>
2021-09-21 23:30:57 -0600

    Add marginal plots for categorical data

--- R/explore.R
+++ R/explore.R
@@ -11,44 +11,72 @@
 # i: colid
 mc_plot_univariate <- function(i, x, dist="ZINB") {
 
-    srv <- x$srv
     opts <- getOption("moose_options")
-    d <- stats::density(x[,i])
-    d_srv <- stats::density(x[srv,i])
-    d_uns <- stats::density(x[!srv,i])
-
-    ylim <- c(0, max(max(d_srv$y), max(d_uns$y)))
-    xlim <- c(0, max(max(d_srv$x), max(d_uns$x)))
-
+    srv <- x$srv
     z <- x[,i]
+    CAT <- is.factor(z) || is.character(z)
+    if (CAT) {
+        z <- droplevels(as.factor(z))
+#        if (any(table(z, srv) == 0L))
+#            stop("Categorical variable has different levels in surveyed vs. unsurveyed")
+    }
     xy <- x[, opts$xy]
-    Col <- rev(grDevices::heat.colors(10))
-    dat <- data.frame(y=x[srv,opts$Ntot], z=x[srv,i])
+    dat <- data.frame(y=x[srv,opts$Ntot], z=z[srv])
     dat <- dat[order(dat$z),]
     m <- zeroinfl2(y ~ z | 1, dat, dist=dist, link='logit')
     dat$zhat <- stats::fitted(m)
 
     op <- graphics::par(mfrow=c(1,3))
-    plot(d, xlim=xlim,
-      ylim=ylim + c(0, 0.2*diff(ylim)), xlab=i, main="Density")
-    graphics::lines(d_srv, col=2)
-    graphics::rug(x[,i], col=1, side=1)
-    graphics::rug(x[srv,i], col=2, side=3)
-    #graphics::lines(d_uns, col=4)
-    graphics::legend("topleft", bty="n", lty=1, col=c(1,2),
-        legend=c("All","Surveyed"))
+    on.exit(graphics::par(op))
 
-    plot(xy, pch=19, col=Col[cut(z, 10)], main="Map",
-        xlab="Longitude", ylab="Latitude",
-        ylim=c(min(xy[,2])-0.2*diff(range(xy[,2])), max(xy[,2])))
-    graphics::legend("bottomleft", bty="n", pch=19, col=Col[c(length(Col),1)],
-      legend=c("High", "Low"))
+    if (CAT) {
+        d <- table(z)/length(srv)
+        d_srv <- table(z[srv])/sum(srv)
+        d_uns <- table(z[!srv])/sum(!srv)
+        ylim <- c(0, max(max(d_srv), max(d_uns)))
 
-    plot(jitter(y) ~ z, dat, main="Total moose response",
-        xlab=i, ylab="Total moose", col="darkgrey", pch=19)
-    graphics::lines(zhat ~ z, dat, col=4, lwd=2)
+        barplot(rbind(d, d_srv), main="Density", beside=TRUE, col=1:2)
+        graphics::legend("topleft", bty="n", fill=c(1,2),
+            legend=c("All","Surveyed"))
 
-    graphics::par(op)
+        Col <- rev(grDevices::hcl.colors(length(d), "Set 2"))
+        plot(xy, pch=19, col=Col[as.integer(z)], main="Map",
+            xlab="Longitude", ylab="Latitude", asp=1,
+            ylim=c(min(xy[,2])-0.2*diff(range(xy[,2])), max(xy[,2])))
+
+        plot(jitter(y) ~ z, dat, main="Total moose response",
+            xlab=i, ylab="Total moose", col="darkgrey", pch=19)
+        graphics::lines(zhat ~ z, dat, col=4, lwd=2)
+
+    } else {
+        d <- stats::density(x[,i])
+        d_srv <- stats::density(x[srv,i])
+        d_uns <- stats::density(x[!srv,i])
+        ylim <- c(0, max(max(d_srv$y), max(d_uns$y)))
+        xlim <- c(0, max(max(d_srv$x), max(d_uns$x)))
+
+        plot(d, xlim=xlim,
+          ylim=ylim + c(0, 0.2*diff(ylim)), xlab=i, main="Density")
+        graphics::lines(d_srv, col=2)
+        graphics::rug(x[,i], col=1, side=1)
+        graphics::rug(x[srv,i], col=2, side=3)
+        #graphics::lines(d_uns, col=4)
+        graphics::legend("topleft", bty="n", lty=1, col=c(1,2),
+            legend=c("All","Surveyed"))
+
+        Col <- rev(grDevices::heat.colors(10))
+        plot(xy, pch=19, col=Col[cut(z, 10)], main="Map",
+            xlab="Longitude", ylab="Latitude", asp=1,
+            ylim=c(min(xy[,2])-0.2*diff(range(xy[,2])), max(xy[,2])))
+        graphics::legend("bottomleft", bty="n", pch=19, col=Col[c(length(Col),1)],
+          legend=c("High", "Low"))
+
+        plot(jitter(y) ~ z, dat, main="Total moose response",
+            xlab=i, ylab="Total moose", col="darkgrey", pch=19)
+        graphics::lines(zhat ~ z, dat, col=4, lwd=2)
+
+    }
+
     invisible(NULL)
 }
 
