764ba8daea464e49cea9f50682760665edead0d3
Peter Solymos <psolymos@gmail.com>
2026-01-24 19:52:22 -0700

    Script
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- inst/simulations/sim-script.R
+++ inst/simulations/sim-script.R
@@ -1,15 +1,26 @@
-MODEL <- "ZINB"
-GRID_SIZE <- 50
-FIXED_SAMPLE_SIZE <- 100
-THETA_NB <- 0.5
-B <- 100
-N <- 25
+# Rscript --vanilla inst/simulations/sim-script.R --model ZIP
 
+library(rconfig)
 library(intrval)
 devtools::load_all()
 
+CONFIG <- rconfig::rconfig()
+str(CONFIG)
+
+DIR <- value(CONFIG$dir, "_tmp/simuls")
+MODEL <- value(CONFIG$model, "P")
+GRID_SIZE <- value(CONFIG$grid, 50)
+FIXED_SAMPLE_SIZE <- value(CONFIG$sample, 250)
+THETA_NB <- value(CONFIG$theta, 2)
+B <- value(CONFIG$B, 100) # should be 500-1000
+N <- value(CONFIG$N, 100) # >200 for sure
+SEED <- value(CONFIG$seed, 0)
+
+pbapply::pboptions(type = "none")
 mc_options(B = B)
 switch_response("total")
+set.seed(SEED)
+m0 <- sim_rsf(dim_x = GRID_SIZE, dim_y = GRID_SIZE) # same for given seed
 
 get_stats <- function(PI, true_vals) {
     is_srv <- PI$data$srv
@@ -48,38 +59,93 @@ theta_nb <- if (MODEL %in% c("NB", "HNB", "ZINB")) {
     NULL
 }
 
-m0 <- sim_rsf(dim_x = GRID_SIZE, dim_y = GRID_SIZE) |>
-    sim_expected(-0.5, 1, -1.5, 0.5)
+if (MODEL == "P") {
+    COEF <- list(a = -0.5, b = 1, a1 = 0, b1 = 0)
+}
+if (MODEL == "NB") {
+    COEF <- list(a = -0.8, b = 1, a1 = 0, b1 = 0)
+}
+if (MODEL == "ZIP") {
+    COEF <- list(a = 0, b = 1.2, a1 = -1, b1 = 0.25)
+}
+if (MODEL == "ZINB") {
+    COEF <- list(a = -0.2, b = 1.2, a1 = -1, b1 = 0.25)
+}
+# depends on MODEL
+m1 <- sim_expected(
+    m0,
+    intercept = COEF$a,
+    slope = COEF$b,
+    intercept1 = COEF$a1,
+    slope1 = COEF$b1
+)
+MT <- matrix(0, nrow(m0), N)
+for (i in seq_len(N)) {
+    # Counts vary by run
+    m1 <- sim_counts(m1, theta.nb = theta_nb, type = TYPE)
+    MT[, i] <- m1$MOOSE_TOTA
+}
+# summary(colSums(MT))
+# summary(colSums(MT > 0)) / nrow(MT)
+# summary(apply(MT, 2, max))
 
 RES <- list()
-for (k in seq_along(N)) {
-    m <- sim_counts(m0, theta.nb = theta_nb, type = TYPE)
-
-    table(m$MOOSE_TOTA)
-    summary(m)
+k <- 1
+FAILED <- 0
+SRV <- 0 * MT
+while (k <= N) {
+    message(MODEL, ": run ", k, " [", Sys.time(), "]")
+    # m <- sim_counts(m0, theta.nb = theta_nb, type = TYPE)
+    # table(m$MOOSE_TOTA)
+    # summary(m)
+    m <- data.frame(m0, MOOSE_TOTA = MT[, k])
 
     surv_id <- sample(nrow(m), FIXED_SAMPLE_SIZE)
+    SRV[, k] <- 0
+    SRV[surv_id, k] <- 1
     m$srv <- FALSE
     m$srv[surv_id] <- TRUE
     m$AREA_KM <- 16
     summary(m)
 
-    ML <- list(
-        "P" = mc_fit_total(m, vars = "z", zi_vars = "z", dist = "P"),
-        "NB" = mc_fit_total(m, vars = "z", zi_vars = "z", dist = "NB"),
-        "ZIP" = mc_fit_total(m, vars = "z", zi_vars = "z", dist = "ZIP"),
-        "ZINB" = mc_fit_total(m, vars = "z", zi_vars = "z", dist = "ZINB")
-    )
-    PIL <- list()
-    for (i in a) {
-        PIL[[i]] <- try(mc_predict_total(
-            model_id = i,
-            ml = ML,
-            x = m,
-            do_boot = TRUE,
-            do_avg = FALSE
-        ))
+    ML <- list()
+    for (i in c("P", "NB", "ZIP", "ZINB")) {
+        ML[[i]] <- try(mc_fit_total(m, vars = "z", zi_vars = "z", dist = i))
     }
-
-    sapply(PIL, get_stats, true_vals = m$MOOSE_TOTA)
+    if (all(!sapply(ML, inherits, "try-error"))) {
+        PL <- list()
+        for (i in names(ML)) {
+            PL[[i]] <- try(mc_predict_total(
+                model_id = i,
+                ml = ML,
+                x = m,
+                do_boot = TRUE,
+                do_avg = FALSE
+            ))
+        }
+        if (all(!sapply(PL, inherits, "try-error"))) {
+            RES[[k]] <- sapply(PL, get_stats, true_vals = m$MOOSE_TOTA)
+            k <- k + 1
+        } else {
+            FAILED <- FAILED + 1
+        }
+    }
 }
+
+fn <- sprintf("%s/moose-sim_model-%s.RData", DIR, MODEL)
+save(
+    m0,
+    MT,
+    SRV,
+    RES,
+    FAILED,
+    MODEL,
+    COEF,
+    THETA_NB,
+    GRID_SIZE,
+    B,
+    N,
+    SEED,
+    file = fn
+)
+quit("no")
