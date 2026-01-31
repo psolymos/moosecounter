71bdbe238461378e776c1473a46ffa06da1f07e1
Peter Solymos <psolymos@gmail.com>
2026-01-30 23:11:16 -0700

    Add hurdle
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- inst/simulations/sim-script.R
+++ inst/simulations/sim-script.R
@@ -10,13 +10,16 @@ str(CONFIG)
 DIR <- value(CONFIG$dir, "_tmp/simuls")
 MODEL <- value(CONFIG$model, "P")
 GRID_SIZE <- value(CONFIG$grid, 50)
-FIXED_SAMPLE_SIZE <- value(CONFIG$sample, 250)
-THETA_NB <- value(CONFIG$theta, 2)
-B <- value(CONFIG$B, 500) # should be 500-1000
-N <- value(CONFIG$N, 100) # >200 for sure
+FIXED_SAMPLE_SIZE <- value(CONFIG$sample, 500)
+THETA_NB <- value(CONFIG$theta, 5)
+B <- value(CONFIG$B, 500) # should be 500-1000, bootstrap iters
+N <- value(CONFIG$N, 100) # >200 for sure, number of sim runs
 SEED <- value(CONFIG$seed, 0)
 
-pbapply::pboptions(type = "none")
+# MOD_LIST <- c("P", "NB", "ZIP", "ZINB")
+MOD_LIST <- c("P", "NB", "ZIP", "ZINB", "HP", "HNB")
+
+# pbapply::pboptions(type = "none")
 mc_options(B = B)
 switch_response("total")
 set.seed(SEED)
@@ -64,15 +67,15 @@ theta_nb <- if (MODEL %in% c("NB", "HNB", "ZINB")) {
 }
 
 if (MODEL == "P") {
-    COEF <- list(a = -0.7, b = 1, a1 = 0, b1 = 0)
+    COEF <- list(a = -0.5, b = 1, a1 = 0, b1 = 0)
 }
 if (MODEL == "NB") {
     COEF <- list(a = -0.8, b = 1, a1 = 0, b1 = 0)
 }
-if (MODEL == "ZIP") {
+if (MODEL %in% c("ZIP", "HP")) {
     COEF <- list(a = 0, b = 1.2, a1 = -1, b1 = 0.25)
 }
-if (MODEL == "ZINB") {
+if (MODEL %in% c("ZINB", "HNB")) {
     COEF <- list(a = -0.2, b = 1.2, a1 = -1, b1 = 0.25)
 }
 # depends on MODEL
@@ -84,11 +87,11 @@ m1 <- sim_expected(
     slope1 = COEF$b1
 )
 MT <- matrix(0, nrow(m0), N)
-for (i in seq_len(N)) {
-    # Counts vary by run
-    m1 <- sim_counts(m1, theta.nb = theta_nb, type = TYPE)
-    MT[, i] <- m1$MOOSE_TOTA
-}
+# for (i in seq_len(N)) {
+#     # Counts vary by run
+#     m1 <- sim_counts(m1, theta.nb = theta_nb, type = TYPE)
+#     MT[, i] <- m1$MOOSE_TOTA
+# }
 # summary(colSums(MT))
 # summary(colSums(MT > 0)) / nrow(MT)
 # summary(apply(MT, 2, max))
@@ -102,7 +105,10 @@ while (k <= N) {
     # m <- sim_counts(m0, theta.nb = theta_nb, type = TYPE)
     # table(m$MOOSE_TOTA)
     # summary(m)
-    m <- data.frame(m0, MOOSE_TOTA = MT[, k])
+
+    # m <- data.frame(m0, MOOSE_TOTA = MT[, k])
+    m <- sim_counts(m1, theta.nb = theta_nb, type = TYPE)
+    MT[, k] <- m$MOOSE_TOTA
 
     surv_id <- sample(nrow(m), FIXED_SAMPLE_SIZE)
     SRV[, k] <- 0
@@ -113,12 +119,13 @@ while (k <= N) {
     summary(m)
 
     ML <- list()
-    for (i in c("P", "NB", "ZIP", "ZINB")) {
+    for (i in MOD_LIST) {
         ML[[i]] <- try(mc_fit_total(m, vars = "z", zi_vars = "z", dist = i))
     }
     if (all(!sapply(ML, inherits, "try-error"))) {
         PL <- list()
-        for (i in names(ML)) {
+        for (i in MOD_LIST) {
+            message("  - PI: ", i)
             PL[[i]] <- try(mc_predict_total(
                 model_id = i,
                 ml = ML,
@@ -143,7 +150,13 @@ while (k <= N) {
     }
 }
 
-fn <- sprintf("%s/moose-sim_model-%s_B-%s.RData", DIR, MODEL, as.character(B))
+fn <- sprintf(
+    "%s/moose-sim_model-%s_n-%s_B-%s.RData",
+    DIR,
+    MODEL,
+    as.character(FIXED_SAMPLE_SIZE),
+    as.character(B)
+)
 save(
     m0,
     MT,
@@ -160,3 +173,15 @@ save(
     file = fn
 )
 quit("no")
+
+
+library(ggplot2)
+k <- 10^4
+mu <- runif(k, 0, 10)
+theta <- 10
+mu2 <- (mu * rgamma(k, theta)) / theta
+ggplot(data.frame(mu = mu, mu2 = mu2), aes(x = mu, y = mu2)) +
+    geom_point() +
+    geom_smooth()
+
+lm(mu2 ~ mu)
